922abfdf76aa84be31d8965b58caa2ddf0d9b11c
Tommy Chen <tchen175@ucsc.edu>
2024-04-06 04:14:06 -0700

    Started on the gpt interpreter

--- gpt_bot/gpt_interpreter.py
+++ gpt_bot/gpt_interpreter.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+import json
 
 #Free method to get historical information from Yahoo! Finance https://pypi.org/project/yfinance/
 """
@@ -8,30 +9,119 @@ Questions:
 """ 
 
 _GPT_MODEL = "gpt-3.5-turbo"
+_TOOLS = [
+    {
+        "type": "function",
+        "function": {
+            "name": "place_order",
+            "description": "Processes the trader's buy or sell orders in the stock market",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "quantity": {
+                        "type": "integer",
+                        "description": "Amount of shares the user wants to buy or sell" 
+                    },
+                    "price": {
+                        "type": "integer",
+                        "description": "The price the user is willing to buy or sell at"
+                    },
+                    "direction":{
+                        "type": "string",
+                        "enum": ["B", "S"],
+                        "description": "Determines if it is a bull or sell order. \
+                                        B represents a buy order, S represents a \
+                                        sell order"
+                    }
+                    
+                },
+                "required": ["quantity", "price", "direction"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "cancel_order",
+            "description": "Cancels one the user's exisiting order",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "order_token": {
+                        "type": "integer",
+                        "description": "The id representing the order id"
+                    },
+                    "quantity_remaining": {
+                        "type": "integer",
+                        "description" : "Amount of shares that are being removed"
+                    },
+                },
+                "required": ["order_token", "quantity_remaining"]
+            },
+        },
+    },
+
+]
 
 class GPTInterpreter:
     """
     An interpreter that generates executable code given market rules and conditions 
     and general guidelines from a user.
     """
-    def __init__(self, market_rules):
+    
+    def __init__(self, market_rules = "none"):
         """Creates an Interpreter with basic rules of the market
 
         Args:
             market_rules: The general guidelines on how orders are made, traded, and canceled
         """
         self.interpretor = OpenAI()
+    
+    def perform_market_action(self, message, client):
+        """
+        Takes in a message from the user which is interpreted by the 
+        LLM in order to call the correct function. 
 
-        completion = self.interpretor.chat.completions.create(
-            model=_GPT_MODEL,
-            message=[
-                {"role": "system", "content": "You are a helpful assistant"},
-                {"role": "user", "content": market_rules}
-            ]
+        args:
+            message: The message
+            client: Client class
+        """
+        from cda_client import Client
+
+        input = [{"role": "user", "content": message}]
+        response = self.interpretor.chat.completions.create(
+            model = _GPT_MODEL,
+            messages = input,
+            tools = _TOOLS,
         )
 
-        print(completion)
+        response_message = response.choices[0].message
+        # list of the funcitons called
+        functions_called = response_message.tool_calls
+
+        available_functions = {
+            "place_order": client.place_order,
+            "cancel_order": client.cancel_order,
+        }
+        # testing purposes
+        function_list_result = []
+        if functions_called:
+
+            for _function in functions_called:
+                
+                function_name = _function.function.name
+                function_list_result.append(function_name)
+                function_to_call = available_functions[function_name]
+                args = json.loads(_function.function.arguments)
+                output = function_to_call(**args)
+                #
+                print(f"Function Name: {function_name}, Args: {args}")
+                print(f"Output: {output}\n")
+        #print(functions_called)
+        # print(function_list_result, args)
+        return function_list_result, args
 
 
+        
 
 
---
+++ test_interpretor.py
@@ -0,0 +1,101 @@
+from gpt_bot.gpt_interpreter import GPTInterpreter
+from cda_client import Client
+from flask import Flask, jsonify
+import logging
+
+
+client = Client()
+gpt = GPTInterpreter()
+
+
+def test_gpt():
+    msg1 = "I want to buy 10 shares of stock for 2 dollars."
+    msg2 = "I want to sell 5 shares of my stock for 1 dollar"
+    msg3 = "Buy 2 shares of stock for 6 bucks"
+    msg4 = "buy 6 shares of stock for 3 dollars"
+    msg5 = "put a sell order of 2 shares of stock for 1 dollar"
+    msg6 = "Sell 5 shares for 4 dollars"
+    msg7 = "I'm considering selling 50 shares at a price of $30 or higher."
+    msg8 = "SELL 6 SHARES OF MY STOCK FOR three bucks"
+    msg9 = "Sell 1 stock for 1 dollar"
+   
+    msgs = [msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9]
+    correct_params = [{"quantity": 10, "price": 2, "direction": "B"},
+                        {"quantity": 5, "price": 1, "direction": "S"},
+                        {"quantity": 2, "price": 6, "direction": "B"},
+                        {"quantity": 6, "price": 3, "direction": "B"},
+                        {"quantity": 2, "price": 1, "direction": "S"},
+                        {"quantity": 5, "price": 4, "direction": "S"},
+                        {"quantity": 50, "price": 30, "direction": "S"},
+                        {"quantity": 6, "price": 3, "direction": "S"},
+                        {"quantity": 1, "price": 1, "direction": "S"}]
+                      
+    correct = 0
+    incorrect = 0
+    for msg, param in zip(msgs, correct_params):
+        correct_quantity = param["quantity"]
+        correct_price = param["price"]
+        correct_direction = param["direction"]
+
+        function, resulting_params = gpt.perform_market_action(msg, client)
+        if function == ["place_order"]:
+            if resulting_params["quantity"] == correct_quantity and resulting_params["price"] == correct_price and resulting_params["direction"] == correct_direction:
+                correct += 1
+            else:
+                incorrect += 1
+
+    # set info logging level
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"   \033[92mCorrect: {correct}, \033[91mIncorrect: {incorrect}")
+            
+    # try:
+    #         assert gpt.perform_market_action(msg2, client) == ["place_order"]
+    #         results.append("Msg2 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg2 Fail")
+
+    # try:
+    #         assert gpt.perform_market_action(msg3, client) == ["place_order"]
+    #         results.append(f"Msg3 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg3 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg4, client) == ["place_order"]
+    #         results.append(f"Msg4 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg4 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg5, client) == ["place_order"]
+    #         results.append(f"Msg4 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg4 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg6, client) == ["place_order"]
+    #         results.append(f"Msg5 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg5 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg7, client) == ["place_order"]
+    #         results.append(f"Msg6 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg6 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg8, client) == ["place_order"]
+    #         results.append(f"Msg7 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg7 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg9, client) == ["place_order"]
+    #         results.append(f"Msg8 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg8 Fail")
+    # try:
+    #         assert gpt.perform_market_action(msg1, client) == ["place_order"]
+    #         results.append(f"Msg9 Pass")  # Store the result if assertion passes
+    # except AssertionError:
+    #         results.append("Msg9 Fail")
+    
+
+
+if __name__ == "__main__":
+    test_gpt()
