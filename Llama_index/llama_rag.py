08c53a2a67ade4dcdd2b44286ec075686ac62b8e
Lily Faris <lilymfaris@gmail.com>
2024-05-01 15:07:52 -0700

    minor query prompt improvement

--- Llama_index/llama_rag.py
+++ Llama_index/llama_rag.py
@@ -133,6 +133,9 @@ else:
 
 # Ask ChatGPT to generate Code
 # Llama-index promptTemplates 
-response = query_engine.query("Write a Python function called active_strategy() that will code the following: " + prompt + ". DO NOT INCLUDE A DESCRIPTION OF THE CODE OR ANYTHING THAT IS NOT THE CODE ITSELF!")
+response = query_engine.query("Write a Python function called active_strategy() that will code the following: " + prompt + 
+                              ".Ensure the function includes any necessary conditional statements, such as 'if', 'elif', or 'else'," + 
+                              "or calculations as needed to achieve the task or answer the question." 
+                              + "DO NOT INCLUDE A DESCRIPTION OF THE CODE OR ANYTHING THAT IS NOT THE CODE ITSELF!")
 script_runner(str(response))
 a = subprocess.run(['python' if (platform.system() == 'Windows') else 'python3', './Llama_index/active_strategy.py'], text=True)
