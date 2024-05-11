ca954d730e80c25ef1f1a07dab28fc506b20774e
william-siegmund <wsiegmun@ucsc.edu>
2024-05-11 12:42:48 -0700

    Removed old unused test files

--- Llama_index/active_strategy.py
+++ Llama_index/active_strategy.py
@@ -3,15 +3,9 @@
 from helper_functions.market_commands import *
 from helper_functions.client_commands import *
 
-#<s>
-import time
-
 def active_strategy():
-    while account_info()["balance"] > 950:
-        CDA_order(3, 3, 'B')
-        time.sleep(0.25)
-    else:
-        print("Sorry, I was not able to implement your strategy. Please rephrase your prompt and try again.")
-#</s>
+    initial_balance = account_info()["balance"]
+    while account_info()["balance"] >= initial_balance / 2:
+        CDA_order(5, 5, 'B')
 if __name__ == "__main__":
     active_strategy()
