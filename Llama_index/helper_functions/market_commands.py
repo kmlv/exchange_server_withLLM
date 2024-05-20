321a3cd06d0a3124d830ecd0e83f1f508c02e648
william-siegmund <wsiegmun@ucsc.edu>
2024-05-20 10:20:41 -0700

    Added code commentation to helper_functions files, added helper function description for get_client_order_history()

--- Llama_index/helper_functions/market_commands.py
+++ Llama_index/helper_functions/market_commands.py
@@ -14,7 +14,18 @@ with open('./market_client/config.toml', 'r') as f:
         _CLIENT_ADDR = config['client']['addr']
         _CLIENT_PORT = config['client']['flask_port']
 
-def CDA_order(shares: int, price: int, direction: str, time_in_force):
+def CDA_order(shares: int, price: int, direction: str, time_in_force: int):
+    """Sends an HTTP request to the Client object with address _CLIENT_ADDR and port _CLIENT_PORT, to place an order in the Exchange server market.
+       To be used by active_strategy() in active_strategy.py whenever it should place an order.
+        Args:
+            shares: number of shares of the placed order
+            price: price of the placed order
+            direction: 'B' for buy order, 'S' for sell order
+            time_in_force: max amount of time the placed order should remain in the market
+        Returns:
+            token: The token id of the placed order. Returns None if the order could not be placed.
+    """
+
     data = {"quantity" : shares, "price" : price, "direction" : direction, "time": time_in_force}
     print("Placing order: ", direction, shares, "@", price, "lasting ", time_in_force, "s at ", time.time(), flush=True)
 
@@ -27,5 +38,10 @@ def CDA_order(shares: int, price: int, direction: str, time_in_force):
 
 
 def CDA_order_cancel(token: int):
+    """Sends an HTTP request to the Client object with address _CLIENT_ADDR and port _CLIENT_PORT, to cancel an order placed by the Client in the Exchange server market.
+       To be used by active_strategy() in active_strategy.py whenever it should cancel an order.
+        Args:
+            token: token id of the order to be cancelled.
+    """
     print("Cancelling order: ", token, time.time(), flush=True)
     resp = requests.request("POST", f"http://{_CLIENT_ADDR}:{_CLIENT_PORT}/cancel/{token}", json=dict())
